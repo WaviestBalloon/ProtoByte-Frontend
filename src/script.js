a1714d1f8198c33b2bb233fcced8742823d90c48
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2023-03-25 20:07:00 +0000

    All done :D

---
+++ src/script.js
@@ -0,0 +1,39 @@
+const ws = new WebSocket("ws://150.136.54.88:80");
+
+function animateCounter(newValue, duration, counterElement) {
+	const startTimestamp = performance.now();
+	const endTimestamp = startTimestamp + duration;
+	let currentValue = parseInt(counterElement.innerText);
+
+	function updateCounter(timestamp) {
+		const timeLeft = endTimestamp - timestamp;
+		const progress = 1 - timeLeft / duration;
+
+		currentValue = Math.round(progress * (newValue - currentValue) + currentValue);
+		counterElement.innerText = currentValue;
+
+		if (timeLeft > 0) {
+			requestAnimationFrame(updateCounter);
+		}
+	}
+
+	requestAnimationFrame(updateCounter);
+}
+
+ws.onopen = () => {
+	console.log("Connected to server successfully");
+
+	document.getElementById("command-count").innerHTML = "0";
+	document.getElementById("message-count").innerHTML = "0";
+	document.getElementById("guild-count").innerHTML = "0";
+	document.getElementById("user-count").innerHTML = "0";
+
+	ws.onmessage = (event) => {
+		console.log(`Received JSON: ${event.data}`);
+		const data = JSON.parse(event.data);
+		animateCounter(data.commandCount, 1000, document.getElementById("command-count"));
+		animateCounter(data.messageCount, 1000, document.getElementById("message-count"));
+		animateCounter(data.guildCount, 1000, document.getElementById("guild-count"));
+		animateCounter(data.userCount, 1000, document.getElementById("user-count"));
+	};
+}
